495c9c352d389355cdddeb8d7ee4f6c8a6fe8dd7
Sujenn Maciel Fiorella Ancco Escobar <sujenn.ancco@tecsup.edu.pe>
2024-11-11 00:36:09 -0500

    Animaciones combinadas

--- app/src/main/java/com/example/lab13animaciones/MainActivity.kt
+++ app/src/main/java/com/example/lab13animaciones/MainActivity.kt
@@ -30,7 +30,8 @@ class MainActivity : ComponentActivity() {
             //AnimatedVisibility()
             //AnimateColor()
             //AnimateSizeAndPosition()
-            AnimatedContent()
+            //AnimatedContent()
+            AnimatedScreen()
         }
     }
 }
